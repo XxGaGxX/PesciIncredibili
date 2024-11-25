d6b393ceb9e01dad2a7cca5ba9c6d3f9d4e7a670
XxGaGxX <diego.vagnini6@gmail.com>
2024-11-25 10:57:32 +0100

    creazione della pagina login

---
+++ src/Componenti/login/login.component.ts
@@ -0,0 +1,26 @@
+import {Component, signal} from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule,MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+
+})
+
+
+
+export class LoginComponent {
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../Componenti/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path : "",
+    component : LoginComponent
+  }
+];
