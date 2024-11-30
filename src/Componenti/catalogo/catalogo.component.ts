c4ca959a338336902b01fa6acfd691e773391657
diego vagnini <diego.vagnini6@gmail.com>
2024-11-30 21:29:31 +0100

    Creato il componente catalogo, iniziata la progettazione dell html e css, aggiunta nel file route

---
+++ src/Componenti/catalogo/catalogo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-catalogo',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalogo.component.html',
+  styleUrl: './catalogo.component.css'
+})
+export class CatalogoComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
-import { LoginComponent } from '../Componenti/login/login.component';
+import { LoginComponent } from '../Componenti/login/login.component'; 
+import { CatalogoComponent } from '../Componenti/catalogo/catalogo.component';
 
 export const routes: Routes = [
   {
     path : "login",
     component : LoginComponent
+  },
+  {
+    path: "catalogo",
+    component : CatalogoComponent
   }
 ];
