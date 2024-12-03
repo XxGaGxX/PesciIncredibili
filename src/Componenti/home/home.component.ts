20a5c234dfad17df05726ee8eead13483db7ed25
diego vagnini <diego.vagnini6@gmail.com>
2024-12-03 22:37:42 +0100

    risoluzione di alcuni bug stilistici della Navbar, della home e del catalogo

--- src/Componenti/home/home.component.ts
+++ src/Componenti/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, MatGridListModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
