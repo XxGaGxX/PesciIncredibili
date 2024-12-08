25db279f2f4983f2eff45164d1b27d4053b91134
diego vagnini <diego.vagnini6@gmail.com>
2024-12-08 21:12:08 +0100

    fine prima parte pagina home

--- src/Componenti/home/home.component.ts
+++ src/Componenti/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, MatGridListModule],
+  imports: [NavbarComponent, MatGridListModule, RouterLink, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
